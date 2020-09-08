87d90ca749500f8f728018367e89386ebc87d5e1
Nagy Gergo <nagygergo@Nagy-MacBook-Pro.local>
2020-09-09 00:14:24 +0200

    url names

--- website/urls.py
+++ website/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name="index"),
-	path('contact.html', views.contact, name="contact"),
-	path('blog.html', views.blog, name="blog"),
-	path('about.html', views.about, name="about"),
+	path('contact', views.contact, name="contact"),
+	path('blog', views.blog, name="blog"),
+	path('about', views.about, name="about"),
 ]
